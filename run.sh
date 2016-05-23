4c7d2eded92202aeeef8a623e1e51f337b9c846e
arj <arj@informatik.uni-kiel.de>
2016-05-23 17:15:08 +0200

    add conversion to run script

--- run.sh
+++ run.sh
@@ -14,5 +14,9 @@ cd ../molab-scalar-time-series
 npm install
 npm update
 nohup node index.js > std.out 2> std.err < /dev/null &
+cd ../molab-time-series-conversion
+npm install
+npm update
+nohup node index.js > std.out 2> std.err < /dev/null &
 cd ../molab-vector-time-series
 nohup python3 server.py > std.out 2> std.err < /dev/null &

53ee3f196824859d2654ed163136a2fa8c5f34ab
arj <arj@informatik.uni-kiel.de>
2016-07-04 10:54:20 +0200

    adcp update

---
+++ run_no_install.bat
@@ -0,0 +1,10 @@
+start /b cmd /c node index.js
+cd ../molab-auth
+start /b cmd /c node index.js
+cd ../molab-scalar-time-series
+start /b cmd /c node index.js
+cd ../molab-time-series-conversion
+start /b cmd /c node index.js
+cd ../molab-vector-time-series
+start /b cmd /c python server.py
+cd ../molab-visualization-gateway
